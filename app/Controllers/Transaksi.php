90180954b287d79ba781c90a3e0870f1e1486747
Khoirunnisa <khoirunnisaputri92@gmail.com>
2022-12-17 11:25:44 +0700

    1

--- app/Controllers/Transaksi.php
+++ app/Controllers/Transaksi.php
@@ -1,11 +1,7 @@
 <?php
 
 namespace App\Controllers;
-
-use App\Models\BarangModel;
-use App\Models\TransaksiModel;
-use App\Models\UserModel;
-use App\Libraries\Mypdf;
+use Dompdf\Dompdf;
 
 class Transaksi extends BaseController
 { 
@@ -13,17 +9,14 @@ class Transaksi extends BaseController
 	{ 
         helper('form');
 		$this->validation = \Config\Services::validation();
-		$this->transaksi = new TransaksiModel();
-		$this->barang = new BarangModel();
-		$this->pembeli = new UserModel();
-		$this->pdf = new Mypdf();
-
+		$this->session = session();
 	}
 
     public function index()
 	{
-		$id = session('id');
-		$transaksi = $this->transaksi->where('id_pembeli', $id)->findAll();  
+		$id = $this->session->get('id');
+		$transaksiModel = new \App\Models\TransaksiModel();  
+		$transaksi = $transaksiModel->where('id_pembeli', $id)->findAll();  
 		return view('transaksi/index',[
 			'transaksis' => $transaksi,  
 		]);
@@ -38,27 +31,29 @@ class Transaksi extends BaseController
 			$errors = $this->validation->getErrors();
 
 			if(!$errors){
+				$transaksiModel = new \App\Models\TransaksiModel();
 				$transaksi = new \App\Entities\Transaksi();
 
+				$barangModel = new \App\Models\BarangModel();
 				$id_barang = $this->request->getPost('id_barang');
 				$jumlah_pembelian = $this->request->getPost('jumlah');
 
-				$barang = $this->barang->find($id_barang);
+				$barang = $barangModel->find($id_barang);
 				$entityBarang = new \App\Entities\Barang();
 				
 				$entityBarang->id = $id_barang;
 
 				$entityBarang->stok = $barang->stok-$jumlah_pembelian;
-				$this->barang->save($entityBarang);
+				$barangModel->save($entityBarang);
 
 				$transaksi->fill($data);
 				$transaksi->status = 0;
-				$transaksi->created_by = session('id');
+				$transaksi->created_by = $this->session->get('id');
 				$transaksi->created_date = date("Y-m-d H:i:s");
 
-				$this->transaksi->save($transaksi);
+				$transaksiModel->save($transaksi);
 
-				$id = $this->transaksi->insertID(); 
+				$id = $transaksiModel->insertID(); 
 
 				return redirect()->to('transaction');
 			}
@@ -67,19 +62,37 @@ class Transaksi extends BaseController
  
     public function invoice(){
 		$id = $this->request->uri->getSegment(2);
-		$transaksi = $this->transaksi->find($id);
+
+		$transaksiModel = new \App\Models\TransaksiModel();
+		$transaksi = $transaksiModel->find($id);
 
 		$userModel = new \App\Models\UserModel();
 		$pembeli = $userModel->find($transaksi->id_pembeli);
 
-		$barang = $this->barang->find($transaksi->id_barang);
-		$data = [
+		$barangModel = new \App\Models\BarangModel();
+		$barang = $barangModel->find($transaksi->id_barang);
+
+		$html = view('transaksi/invoice',[
 			'transaksi'=> $transaksi,
 			'pembeli' => $pembeli,
 			'barang' => $barang,
-		];
+		]); 
  
         $filename = date('y-m-d-H-i-s'). '-invoice';
-		$this->pdf->generate('transaksi/invoice',$data,$filename);
+
+        // instantiate and use the dompdf class
+        $dompdf = new Dompdf();
+
+        // load HTML content
+        $dompdf->loadHtml($html);
+
+        // (optional) setup the paper size and orientation
+        $dompdf->setPaper('A4', 'potrait');
+
+        // render html as PDF
+        $dompdf->render();
+
+        // output the generated pdf
+        $dompdf->stream($filename); 
     }
 }
